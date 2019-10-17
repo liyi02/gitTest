ab4926bf134467e3557d514a6df315b075a6fb0b
Yi Li <liyi02@lianjia.com>
2019-10-18 00:14:52 +0800

    branch2 第五次提交

--- GitTest/GitTest/ViewController.m
+++ GitTest/GitTest/ViewController.m
@@ -25,8 +25,12 @@
     // 第九次提交
     // branch2 第一次提交
     // branch2 第二次提交
+<<<<<<< HEAD
     // branch2 第三次提交
     // branch2 第四次提交
+=======
+    // branch2 第五次提交
+>>>>>>> branch2 第五次提交
     // Do any additional setup after loading the view, typically from a nib.
 }
 
