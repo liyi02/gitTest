bf2f896673359e11c3a133abf9309a53b80eacc9
Yi Li <liyi02@lianjia.com>
2020-08-17 12:00:20 +0800

    branch1第六次提交

--- GitTest/GitTest/AppDelegate.m
+++ GitTest/GitTest/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // 第四次提交
+    // branch1第六次提交
     // Override point for customization after application launch.
     return YES;
 }
