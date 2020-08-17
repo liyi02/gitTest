e4440d68d64be3a4df20d54cc404dacc9ef44c9b
Yi Li <liyi02@lianjia.com>
2020-08-17 12:10:54 +0800

    branch1第十次提交

--- GitTest/GitTest/main.m
+++ GitTest/GitTest/main.m
@@ -11,6 +11,7 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        // branch1第十次提交
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
